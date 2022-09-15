4fc61899d226b97830feeb798a2881e4fbb6d427
Ali Faisal <libmacali@gmail.com>
2022-09-15 20:42:26 +0400

    Updated word giot to git in title

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Learning Giot'),
+      home: const MyHomePage(title: 'Learning Git'),
     );
   }
 }
